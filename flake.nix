b24fde502147ea9489af64ea99938ee51ad09ea1
steledama <stefano.pompa@gmail.com>
2023-11-19 19:03:09 +0100

    first upload, nixos base

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url ="nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      lib = nixpkgs.lib;
+      system = "x86_64-linux";
+    in {
+      nixosConfigurations = {
+        nixos = lib.nixosSystem {
+          inherit system;
+          modules = [./configuration.nix];
+        };
+        };
+    };
+}
